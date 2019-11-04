dbb18b31b09b8eb545b613e6b9ebd99024e95b21
stewie <siegerx3-games@yahoo.de>
2019-11-04 16:44:10 +0100

    Fixed html for about page

--- src/app/components/main/about/about.module.ts
+++ src/app/components/main/about/about.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { SharedModule } from 'primeng/components/common/shared';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
-import { SharedModule } from 'primeng/components/common/shared';
+
 
 @NgModule({
   declarations: [AboutComponent],
   imports: [
     CommonModule,
     AboutRoutingModule,
+    CardModule,
     SharedModule
   ]
 })
