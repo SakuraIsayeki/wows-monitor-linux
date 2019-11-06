d5b12389a70bc6b963bc2287d095911753b402dd
stewie <siegerx3-games@yahoo.de>
2019-11-06 01:14:54 +0100

    Version bump, added version to about page

--- src/app/components/main/about/about.component.ts
+++ src/app/components/main/about/about.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from '../../base.component';
+import { appConfig } from 'src/config/app.config';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html'
 })
 export class AboutComponent extends BaseComponent implements OnInit {
+
+  get version(){
+    return appConfig.version;
+  }
 
   constructor() {
     super();
