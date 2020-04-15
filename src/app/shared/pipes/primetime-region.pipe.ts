19bb9b047cce9dd5708e8ce878fd23cd7ec54f8c
stewie <siegerx3@outlook.de>
2020-04-15 22:12:41 +0200

    Fixed Season 9 Primetime

--- src/app/shared/pipes/primetime-region.pipe.ts
+++ src/app/shared/pipes/primetime-region.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Region } from 'src/app/generated/models';
 
 const map = [{
-  seasons: [6, 7, 8],
+  seasons: [6, 7, 8, 9, 10],
   primetimes: [
     {
       primetime: [0, 1, 2, 3],
