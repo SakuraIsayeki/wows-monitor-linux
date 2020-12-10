19c0a9fdb0192c98f00d1bfc4a93d805ad4be4d4
stewie <siegerx3@outlook.de>
2020-12-10 22:50:58 +0100

    Add error for empty arena info

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { of } from 'rxjs';
 import { Config } from 'src/config/config';
 import { MatchGroup, Region, TempArenaInfo } from '../generated/models';
 import { StatsService } from '../generated/services';
+import { LoggerService, LoggerServiceToken } from '../interfaces/logger.service';
 
 @Injectable()
 export class ApiService {
@@ -9,7 +11,8 @@ export class ApiService {
   lastInfo: string;
   static lastRegion: Region;
 
-  constructor(private statsService: StatsService, private config: Config) { }
+  constructor(private statsService: StatsService, private config: Config, @Inject(LoggerServiceToken) private logger: LoggerService) {
+  }
 
   resendState() {
     this.sendStats(this.lastInfo, ApiService.lastRegion).subscribe();
@@ -21,6 +24,10 @@ export class ApiService {
     const tempArenaInfo = JSON.parse(tempArenaInfoJson) as TempArenaInfo;
     if (this.config.forcePVP) {
       tempArenaInfo.useMatchGroup = MatchGroup.PVP;
+    }
+    if (tempArenaInfo == null) {
+      this.logger.error('StatsService', 'Error when reading tempArenaInfo. It\'s empty');
+      return of(null);
     }
     return this.statsService.statsSendStats({ token: this.config.signalRToken, body: tempArenaInfo });
   }
