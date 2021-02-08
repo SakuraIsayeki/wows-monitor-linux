e250ac6f49588aebc800764a41569244a17418ad
stewie <siegerx3@outlook.de>
2021-02-08 03:46:36 +0100

    Bump version

--- src/environments/environment.desktop.ts
+++ src/environments/environment.desktop.ts
@@ -4,7 +4,7 @@ export const environment: Environment = {
   production: false,
   browser: false,
   desktop: true,
-  apiUrl: 'https://api.wows-monitor.com',
+  apiUrl: 'http://localhost:5000',
   gaCode: 'UA-151253199-8',
-  appUrl: 'https://app.wows-monitor.com'
+  appUrl: 'http://localhost:4201'
 };
